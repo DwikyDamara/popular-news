5dcd52086befce3566518e5fde26abc0d4957a43
Dwiky Damara <damaradwiky6@gmail.com>
2023-03-19 18:38:27 +0700

    Change to oop by adding class

--- PopularNews/__init__.py
+++ PopularNews/__init__.py
@@ -1,37 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-description = "This app show top 5 news in Indonesia based from detik.com"
 
+class NewsScraper:
+    def __init__(self, url):
+        self.description = "This app show top 5 news in Indonesia based from detik.com"
+        self.result = None
+        self.url = url
 
-def data_extraction():
-    content = requests.get("https://detik.com/")
-    soup = BeautifulSoup(content.text, "html.parser")
-    top5 = soup.find("div", {"class": "box cb-mostpop"})
-    titles = []
-    for article in top5.find_all("article"):
-        title = article.find("h3", class_="media__title").find("a").text.strip()
-        titles.append(title)
+    def data_extraction(self):
+        content = requests.get(self.url)
+        soup = BeautifulSoup(content.text, "html.parser")
+        top5 = soup.find("div", {"class": "box cb-mostpop"})
+        titles = []
+        for article in top5.find_all("article"):
+            title = article.find("h3", class_="media__title").find("a").text.strip()
+            titles.append(title)
 
-    urls = []
-    for article in top5.find_all("article"):
-        url = article.find("h3", class_="media__title").find("a")
-        urls.append(url.get("href"))
+        urls = []
+        for article in top5.find_all("article"):
+            url = article.find("h3", class_="media__title").find("a")
+            urls.append(url.get("href"))
 
-    hasil = dict()
-    hasil["Titles"] = titles
-    hasil["Firsturl"] = urls
-    return hasil
+        hasil = dict()
+        hasil["Titles"] = titles
+        hasil["Urls"] = urls
+        return hasil
 
+    def data_displaying(self):
+        print("Berita Terpopuler!")
+        result = self.data_extraction()
+        for i in range(0, 5):
+            print(f'{i+1}. {result["Titles"][i]}')
+            print(f'Link: {result["Urls"][1]}')
 
-def data_displaying(result):
-    print("Berita Terpopuler!")
-    for i in range(0, 5):
-        print(f'{i+1}. {result["Titles"][i]}')
-        print(f'Link: {result["Firsturl"][1]}')
+    def run(self):
+        self.data_extraction()
+        self.data_displaying()
 
 
 if __name__ == "__main__":
-    print(description)
-    result = data_extraction()
-    data_displaying(result)
+    DetikNewsScraper = NewsScraper("https://detik.com/")
+    print(DetikNewsScraper.description)
+    DetikNewsScraper.run()
+    # print(description)
+    # result = data_extraction()
+    # data_displaying(result)
