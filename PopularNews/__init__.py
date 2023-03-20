033fabbe3b2e52c30886f03783081b5ba9c8c8e1
Dwiky Damara <damaradwiky6@gmail.com>
2023-03-20 13:53:54 +0700

    Back to OOP

--- PopularNews/__init__.py
+++ PopularNews/__init__.py
@@ -1,41 +1,66 @@
-import requests # Import requests package to send HTTP requests
-from bs4 import BeautifulSoup # Import BeautifulSoup package to parse HTML
+import requests
+from bs4 import BeautifulSoup
+"""
+Method = Funtion
+Field/Attribute = Variable
+Contructor = The method that is called the first time the object is created. Use to declare variables in this class
+"""
 
-description = "This app show top 5 news in Indonesia based from detik.com"
+class Scraper:
+    def __init__(self, url, description):  #Constructor
+        self.description = description
+        self.result = None
+        self.url = url
 
-# Create function named data_exctraction
-def data_extraction():
-    # Using request package to get html code and parser it with BeautifulSoup4
-    content = requests.get("https://detik.com/")
-    soup = BeautifulSoup(content.text, "html.parser")
-    top5 = soup.find("div", {"class": "box cb-mostpop"})
+    def data_extraction(self):
+        pass
 
-    titles = [] # Create an empty list to store article titles
-    for article in top5.find_all("article"):
-        title = article.find("h3", class_="media__title").find("a").text.strip()
-        titles.append(title)
+    def data_displaying(self):
+        pass
 
-    urls = [] # Create an empty list to store article URLs
-    for article in top5.find_all("article"):
-        url = article.find("h3", class_="media__title").find("a")
-        urls.append(url.get("href"))
+    def run(self):
+        self.data_extraction()
+        self.data_displaying()
 
-    # Create a dictionary with "titles" and "urls" lists as values, and return it
-    hasil = dict()
-    hasil["Titles"] = titles
-    hasil["Urls"] = urls
-    return hasil
+class NewsScraper(Scraper):
+    def __init__(self, url):
+        super(NewsScraper, self).__init__( url, "Will Show Top 5 News In Indonesia")
 
-# Create function named data_displaying
-def data_displaying(result):
-    print("Berita Terpopuler!")
-    for i in range(0, 5): # Loop over the first 5 articles
-        print(f'{i+1}. {result["Titles"][i]}')
-        print(f'Link: {result["Urls"][1]}')
+    def data_extraction(self):
+        content = requests.get(self.url)
+        soup = BeautifulSoup(content.text, "html.parser")
+        top5 = soup.find("div", {"class": "box cb-mostpop"})
+        titles = []
+        for article in top5.find_all("article"):
+            title = article.find("h3", class_="media__title").find("a").text.strip()
+            titles.append(title)
 
-# will only execute if runs as a script
+        urls = []
+        for article in top5.find_all("article"):
+            url = article.find("h3", class_="media__title").find("a")
+            urls.append(url.get("href"))
+
+        hasil = dict()
+        hasil["Titles"] = titles
+        hasil["Urls"] = urls
+        return hasil
+
+    def data_displaying(self):
+        print("Berita Terpopuler!")
+        result = self.data_extraction()
+        for i in range(0, 5):
+            print(f'{i+1}. {result["Titles"][i]}')
+            print(f'Link: {result["Urls"][1]}')
+
+class ArticleScraper(Scraper):
+    def __init__(self, url):
+        super(ArticleScraper, self).__init__(url, "\nNot Yet Released!!Stay Tuned!!")
+
 if __name__ == "__main__":
-    print(description)
-    result = data_extraction()
-    data_displaying(result)
+    DetikNewsScraper = NewsScraper("https://detik.com/")
+    print(DetikNewsScraper.description)
+    DetikNewsScraper.run()
 
+    MediumArticleScraper = ArticleScraper("https://medium.com/")
+    print(MediumArticleScraper.description)
+    MediumArticleScraper.run()
